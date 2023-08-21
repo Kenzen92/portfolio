1da1fa92852e1b39b7fc170a241b3bbb38dbd914
Kenzen92 <jamespeterkenny@gmail.com>
2023-08-22 00:36:15 +0100

    added tech stack logos

--- src/components/sections/section2/Section2.tsx
+++ src/components/sections/section2/Section2.tsx
@@ -3,6 +3,8 @@ import { motion } from 'framer-motion';
 import './Section2.css'
 import indexPage from '/index-page.png'
 import SectionImage from '../../images/section-image/SectionImage';
+import desktopDashboard from '/desktop-dashboard.png'
+import logos from '/logos.png'
 
 
 const Section2: React.FC = () => { 
@@ -19,66 +21,28 @@ const Section2: React.FC = () => {
             Welcome to my web development portfolio! In this showcase, I'm excited to present an innovative web application I've crafted using Flask, vanilla JavaScript, and CSS. This functional prototype, hosted on an AWS EC2 instance powered by Ubuntu, is seamlessly served by Nginx and Gunicorn, ensuring smooth user experiences.
           </p>
           </motion.div>
+
+          <SectionImage src={indexPage} alt="Grayscale Image" />
           
           <motion.div className="section-2-text-container"
           initial={{ scale: 0.6 }}
           whileInView={{ scale: 1, transition: { type: 'spring', stiffness: 100, damping: 10 } }}
           >
-          <h2>Key Features:</h2>
-          <ul>
-            <li>User Authentication with Google OAuth2:</li>
-            <p>
-              This application incorporates secure account creation and authentication using Google OAuth2. Teachers can effortlessly create accounts, ensuring a seamless login experience and safeguarding user data.
-            </p>
-
-            <li>Custom Calendar Integration:</li>
-            <p>
-              A standout feature of this app is its custom-built calendar system. Teachers can easily schedule and manage online classes with students, facilitating efficient time management and organization.
-            </p>
-
-            <li>File Upload and Download:</li>
-            <p>
-              Facilitating effective teaching, the platform enables teachers to upload and share study materials with students both before and after classes. This file-sharing functionality streamlines the learning process and enhances collaboration.
-            </p>
-
-            <li>Assignment Management:</li>
-            <p>
-              Empowering teachers, the application provides a dedicated feature to assign and track homework. Teachers can conveniently distribute assignments to students and monitor their progress, fostering an interactive learning environment.
-            </p>
-          </ul>
+          <h2>Key Features</h2>
+          <p>This app offers secure Google OAuth2-based user authentication and a custom calendar system for streamlined class scheduling. Teachers can upload and share study materials, manage assignments, and monitor progress effectively, enhancing the learning experience.</p>
           </motion.div>
 
-          <SectionImage src={indexPage} alt="Grayscale Image" />
+          
         </div>
         <div className="row-2">
-        <SectionImage src={indexPage} alt="Grayscale Image" />
+        <SectionImage src={desktopDashboard} alt="Grayscale Image" />
         <motion.div className="section-2-text-container"
         initial={{ scale: 0.8 }}
         whileInView={{ scale: 1, transition: { type: 'spring', stiffness: 100, damping: 10 } }}
         >
-        <h2>Technology Stack:</h2>
-        <ul>
-          <li>Backend Framework: Flask</li>
-          <p>
-            Leveraging the power of Flask, this application's backend ensures smooth data handling, efficient routing, and robust logic implementation.
-          </p>
-
-          <li>Frontend Frameworks: Vanilla JavaScript and CSS</li>
-          <p>
-            Employing Vanilla JavaScript and CSS, the frontend delivers a clean and user-friendly interface. The combination of these technologies guarantees responsive design and an engaging user experience.
-          </p>
-
-          <li>Hosting Environment: AWS EC2 and Ubuntu</li>
-          <p>
-            The application is hosted on an AWS EC2 instance running Ubuntu. This environment ensures scalability, reliability, and optimized performance, essential for handling users and data seamlessly.
-          </p>
-
-          <li>Server Configuration: Nginx and Gunicorn</li>
-          <p>
-            The application is efficiently served by Nginx, acting as a reverse proxy, while Gunicorn manages the Python application server. This configuration guarantees swift loading times and reliable delivery of content.
-          </p>
-        </ul>
-
+        <h2>Technology Stack</h2>
+        <p>The app's backend utilizes Flask, ensuring efficient data handling, while the frontend employs Vanilla JavaScript and CSS for a responsive interface. It's hosted on AWS EC2 with Ubuntu, ensuring scalability. Nginx serves as a reverse proxy, and Gunicorn manages the Python application server, guaranteeing fast and reliable content delivery.</p>
+        <SectionImage src={logos} alt="flask, nginx and gunicorn logo" />
         <div className="section-2-link">
           <a href="https://kennysolutions.com/" target="_blank" rel="noopener noreferrer">
             <button className="kenny-solutions-button">
