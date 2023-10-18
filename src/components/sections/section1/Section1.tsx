9e71e04e5e9618fadbf6337481b13393ce1ac8bb
kenzen92 <jamespeterkenny@googlemail.com>
2023-10-18 10:46:17 +0100

    updatyed text and added a favicon

--- src/components/sections/section1/Section1.tsx
+++ src/components/sections/section1/Section1.tsx
@@ -7,8 +7,12 @@ const Section1: React.FC = () => {
   return (
     <div className="section-1-container">  
         <h1>James Kenny Portfolio</h1>    
-        <p>Welcome to my portfolio. My name is James, I'm a 30 year old developer from the UK, but now living in Porto, Portugal.</p>
-        <p>This site consists of two web applications. Scroll down to read about the technology powering them, and follow the links to view the applications or the respective github pages.</p>
+        <p>Welcome to my portfolio. I'm a computer science graduate and junior developer hoping to find a position in the
+          industry where I can develop my skills and make a significant difference to the projects I work on.</p>
+        <br></br>
+        <p>This portfolio consists of two web applications. One full-stack application I designed and built to help online teachers manage their schedules, classes and students. 
+          The second app is a weather app heavily inspired by the Apple weather app, and build using React. 
+          Continue reading to discover the technology powering them, and try them out for yourself.</p>
     </div>
   );
 };
